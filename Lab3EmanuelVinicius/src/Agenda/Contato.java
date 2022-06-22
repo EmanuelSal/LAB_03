5c8effe090370778a95665af5c3c6e969e910913
evlima <limaemanuel06@gmail.com>
2022-06-21 23:21:27 -0300

    Update Contato.java

--- Lab3EmanuelVinicius/src/Agenda/Contato.java
+++ Lab3EmanuelVinicius/src/Agenda/Contato.java
@@ -27,18 +27,31 @@ public class Contato {
 	private String telefone;
 	
 	/**
+	 * 
+	 */
+	private String[] addTags;
+	
+	/**
 	 * Construtor que recebe o nome, sobrenome e numero de telefone do contato
 	 * 
 	 * @param nome do contato
 	 * @param sobrenome do contato
 	 * @param numero de telefone do contato
+	 * 
 	 */
 	public Contato(String nome, String sobrenome, String numero) {
+		
+		this.addTags = new String[5];
 		this.nome = nome;
 		this.sobrenome = sobrenome;
 		this.telefone = numero;
 	}
 	
+	public Contato() {
+		
+		// TODO Auto-generated constructor stub
+	}
+
 	/**
 	 * 
 	 * @param nome da contato
@@ -65,8 +78,53 @@ public class Contato {
 	public String getNumero() {
 		return this.telefone;
 	}
-	public String toString( ) {
+	
+	/**
+	 * 
+	 */
+	public String toString() {
 		return this.nome + " " + this.sobrenome;
 		
 	}
-}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null) {
+			return false;
+		} if(!obj.getClass().equals(this.getClass())) {
+			return false;
+		} 
+		Contato contato = (Contato) obj;
+		
+		return this.nome.equals(contato.nome) && this.sobrenome.equals(contato.sobrenome);
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getTag() {
+		
+		String addTag = "";  
+		for(int indice = 0; indice < addTags.length; indice++) {
+			
+			if(addTags[indice] != null) {
+				addTag += addTags[indice] + " ";
+			}
+		} return addTag.trim();
+	}
+	/**
+	 * 
+	 * @param tag
+	 * @param posicao
+	 */
+	public void colocaTag(String tag, int posicao) {
+		this.addTags[posicao - 1] = tag;
+	}
+	
+	public int codigoHash() {
+		return this.nome.hashCode() + this.sobrenome.hashCode();
+	}
+}	
